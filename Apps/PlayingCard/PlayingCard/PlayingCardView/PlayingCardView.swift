53e72ab9fe0fb2fb06435f9cd987a61f801a6c20
maksim_mitrofanov <keyseymiroff@gmail.com>
2023-02-17 00:38:01 +0400

    PlayingCardViewAdded

---
+++ Apps/PlayingCard/PlayingCard/PlayingCardView/PlayingCardView.swift
@@ -0,0 +1,25 @@
+//
+//  PlayingCardView.swift
+//  PlayingCard
+//
+//  Created by Максим Митрофанов on 16.02.2023.
+//
+
+import UIKit
+
+class PlayingCardView: UIView {
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setup()
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        setup()
+    }
+    
+    func setup() {
+        
+    }
+}
