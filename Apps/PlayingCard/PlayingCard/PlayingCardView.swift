8adba72bf7aca6e2aa53064fd64c82df8463d96f
maksim_mitrofanov <keyseymiroff@gmail.com>
2023-02-16 18:09:50 +0400

    Lecture 6 code (5%)

---
+++ Apps/PlayingCard/PlayingCard/PlayingCardView.swift
@@ -0,0 +1,15 @@
+//
+//  PlayingCardView.swift
+//  PlayingCard
+//
+//  Created by Максим Митрофанов on 16.02.2023.
+//
+
+import UIKit
+
+class PlayingCardView: UIView {
+
+    override func draw(_ rect: CGRect) {
+
+    }
+}
