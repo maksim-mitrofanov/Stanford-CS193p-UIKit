9bfb26bdc6a6d59b6ddb1b6fc1812c860d9ed4eb
maksim_mitrofanov <keyseymiroff@gmail.com>
2023-02-23 15:10:50 +0400

    Adding Concentration Gam

---
+++ SET Game/SET Game/Model/Theme.swift
@@ -0,0 +1,13 @@
+//
+//  Theme.swift
+//  Concentration
+//
+//  Created by Максим Митрофанов on 29.01.2023.
+//
+
+import UIKit
+
+struct Theme {
+    let emojis: [String]
+    let backgroundColor: UIColor
+}
