63a1767fc2a78b4a417b9ae5a245051a7d65a973
maksim_mitrofanov <keyseymiroff@gmail.com>
2023-02-24 21:10:47 +0400

    Lecture 9 code + slides + demo

---
+++ Apps/Curiosity Mission/Curiosity Mission/ProgrammaticScrollViewViewController.swift
@@ -0,0 +1,70 @@
+//
+//  ProgrammaticScrollViewViewController.swift
+//  Curiosity Mission
+//
+//  Created by Максим Митрофанов on 24.02.2023.
+//
+
+import UIKit
+
+class ProgrammaticScrollViewViewController: UIViewController {
+    
+    @IBOutlet private weak var scrollView: UIScrollView! {
+        didSet {
+            scrollView.addSubview(imageView)
+            scrollView.minimumZoomScale = 1/10
+            scrollView.maximumZoomScale = 0.5
+        }
+    }
+    private var imageView = UIImageView()
+    
+    private var displayedImage: UIImage? {
+        get { imageView.image }
+        set {
+            imageView.image = newValue
+            imageView.sizeToFit()
+            scrollView.contentSize = imageView.frame.size
+        }
+    }
+    
+    private var imageURL: URL? {
+        didSet {
+            displayedImage = nil
+            if view.window != nil {
+                fetchImage()
+            }
+        }
+    }
+    
+    
+    
+    
+    private func fetchImage() {
+       displayedImage = UIImage(named: "MarsFromTop")
+    }
+    
+    
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        scrollView.delegate = self
+        if displayedImage == nil {
+            fetchImage()
+        }
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        if imageView.image == nil {
+            fetchImage()
+        }
+    }
+}
+
+extension ProgrammaticScrollViewViewController: UIScrollViewDelegate {
+    func viewForZooming(in scrollView: UIScrollView) -> UIView? {
+        return imageView
+    }
+}
