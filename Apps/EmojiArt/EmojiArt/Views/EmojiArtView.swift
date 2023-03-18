cff7ce87e3fa41184352cee9806b7a5f4c05ac78
maksim_mitrofanov <keyseymiroff@gmail.com>
2023-03-19 02:15:54 +0400

    Lecture 11 Demo added

--- Apps/EmojiArt/EmojiArt/Views/EmojiArtView.swift
+++ Apps/EmojiArt/EmojiArt/Views/EmojiArtView.swift
@@ -31,7 +31,7 @@ class EmojiArtView: UIView {
 
 extension EmojiArtView: UIDropInteractionDelegate {
     func dropInteraction(_ interaction: UIDropInteraction, canHandle session: UIDropSession) -> Bool {
-        session.canLoadObjects(ofClass: String.self)
+        session.canLoadObjects(ofClass: String.self) || session.canLoadObjects(ofClass: UIImage.self)
     }
     
     func dropInteraction(_ interaction: UIDropInteraction, sessionDidUpdate session: UIDropSession) -> UIDropProposal {
@@ -39,11 +39,21 @@ extension EmojiArtView: UIDropInteractionDelegate {
     }
     
     func dropInteraction(_ interaction: UIDropInteraction, performDrop session: UIDropSession) {
-        session.loadObjects(ofClass: String.self) { itemProviders in
-            let dropPoint = session.location(in: self)
-            
-            for string in itemProviders {
-                self.addLabel(with: string, at: dropPoint)
+        if session.canLoadObjects(ofClass: UIImage.self) {
+            session.loadObjects(ofClass: UIImage.self) { itemProviders in
+                DispatchQueue.main.async { [weak self] in
+                    self?.imageToDisplay = itemProviders.first as? UIImage
+                }
+            }
+        }
+         
+        else if session.canLoadObjects(ofClass: String.self) {
+            session.loadObjects(ofClass: String.self) { itemProviders in
+                let dropPoint = session.location(in: self)
+                
+                for string in itemProviders {
+                    self.addLabel(with: string, at: dropPoint)
+                }
             }
         }
     }
